f0c85591edeb446a65fd170230fa43f84b461bcd
uramuhin <urasfinks@yandex.ru>
2022-05-18 22:04:31 +0300

    Bootstrap 5.2.0-beta1 add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RecComponent } from './rec/rec.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { XsdCreatorComponent } from './xsd-creator/xsd-creator.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const appRoutes: Routes = [
+  {path: 'xsd-creator', component: XsdCreatorComponent},
+  {path: '', component: HomePageComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecComponent
+    RecComponent,
+    HomePageComponent,
+    XsdCreatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
