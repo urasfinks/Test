d7be263f65ac028679481d18b96fe38b063a76c9
sfinks <urasfinks@yandex.ru>
2022-05-26 17:52:35 +0300

    Highlight xml (temporary)

--- src/app/xsd-creator/xsd-creator.component.ts
+++ src/app/xsd-creator/xsd-creator.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {XsdElement} from "./class/XsdElement";
 import {HistoryService} from "./history.service";
 
+
 @Component({
   selector: 'app-xsd-creator',
   templateUrl: './xsd-creator.component.html',
@@ -11,7 +12,7 @@ export class XsdCreatorComponent implements OnInit {
 
   history: Array<XsdElement> = [];
   last: string = "";
-
+  xsd: string = "";
 
   constructor(private service: HistoryService) { }
 
@@ -36,6 +37,43 @@ export class XsdCreatorComponent implements OnInit {
   }
 
   onChange(data: string){
-    console.log(data);
+    let json: XsdElement = JSON.parse(data);
+
+    this.xsd = this.prettifyXml2(this.rec(json),"    ");
+    console.log(this.xsd);
   }
+
+  rec(el: XsdElement){
+
+    let ret = "";
+    if(el.listAttribute.length > 0){
+      if(el.typeElement == 1){//[attribute + content simple] Element -> complexType -> simpleContent -> extension -> attribute
+        ret += "1";
+      } else if(el.typeElement == 2){//[attribute + content array] -> Element -> complexContent -> extension [complexType seq] -> attribute
+        ret += "2";
+      }
+    }else{
+      if(el.typeElement == 1){// [content] Element -> simpleType -> restriction
+        ret += "<xs:element name=\""+el.name+"\"><r>1</r></xs:element>";
+        //  ret += "<root><p>1</p></root>";
+      } else if(el.typeElement == 2){// ...
+        ret += "4";
+      }
+    }
+    return ret;
+  }
+
+  prettifyXml2(xml: string, tab: string) { // tab = optional indent value, default is tab (\t)
+    var formatted = '', indent= '';
+    tab = tab || '\t';
+    xml.split(/>\s*</).forEach(function(node) {
+      if (node.match( /^\/\w/ )) indent = indent.substring(tab.length); // decrease indent by one 'tab'
+      formatted += indent + '<' + node + '>\r\n';
+      if (node.match( /^<?\w[^>]*[^\/]$/ )) indent += tab;              // increase indent
+    });
+    return formatted.substring(1, formatted.length-3);
+  }
+
 }
+
+
